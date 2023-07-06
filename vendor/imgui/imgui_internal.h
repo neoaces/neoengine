c8a2a1b705cb8a2f266579ce376ccb5765e59148
Anzle Chavez <anzlemchavez@gmail.com>
2023-07-06 00:15:49 -0400

    remove intel intrinsics

--- vendor/imgui/imgui_internal.h
+++ vendor/imgui/imgui_internal.h
@@ -56,13 +56,6 @@ Index of this file:
 #include <stdlib.h>     // NULL, malloc, free, qsort, atoi, atof
 #include <math.h>       // sqrtf, fabsf, fmodf, powf, floorf, ceilf, cosf, sinf
 #include <limits.h>     // INT_MIN, INT_MAX
-
-// Enable SSE intrinsics if available
-#if (defined __SSE__ || defined __x86_64__ || defined _M_X64 || (defined(_M_IX86_FP) && (_M_IX86_FP >= 1))) && !defined(IMGUI_DISABLE_SSE)
-#define IMGUI_ENABLE_SSE
-#include <immintrin.h>
-#endif
-
 // Visual Studio warnings
 #ifdef _MSC_VER
 #pragma warning (push)
